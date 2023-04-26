d4c83f85664900ab5bda843cdabda0223201f652
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2023-04-26 23:22:04 +0200

    CNAME file

--- .eleventy.js
+++ .eleventy.js
@@ -106,6 +106,7 @@ module.exports = function (eleventyConfig) {
 
   //eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   // WebC
   eleventyConfig.addPlugin(eleventyWebcPlugin, {
