0a1f193a705d21695614421c1aeee3fae25d684c
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2023-04-27 20:44:19 +0200

    feed

--- .eleventy.js
+++ .eleventy.js
@@ -47,6 +47,10 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.addFilter("dateToIso", (dateString) => {
+    return new Date(dateString).toISOString();
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
@@ -107,7 +111,6 @@ module.exports = function (eleventyConfig) {
   //eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy({ "src/CNAME": "public/CNAME" });
-  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
   // WebC
   eleventyConfig.addPlugin(eleventyWebcPlugin, {
