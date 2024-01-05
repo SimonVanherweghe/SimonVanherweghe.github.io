288f2b75da36dd23b94115dbdb4c014409fa1e80
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2024-01-05 06:30:50 +0100

    archive

--- src/content/config.js
+++ src/content/config.js
@@ -17,7 +17,8 @@ const archiveCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
-    image: z.string().optional(),
+    date: z.date(),
+    permalink: z.string(),
   }),
 });
 // 3. Export a single `collections` object to register your collection(s)
