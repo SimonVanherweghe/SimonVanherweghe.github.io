c4b28e2605541f62d7fd4d8b3ea4048a77e31e0e
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2024-01-06 07:11:13 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://donebysimon.be",
+  integrations: [sitemap()]
 });
