c567fbcea2964757b86302dae9ba91b2f1b3be5a
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2025-01-07 22:43:20 +0100

    og data

---
+++ src/pages/og/[...route].ts
@@ -0,0 +1,30 @@
+// src/pages/open-graph/[...route].ts
+import { getCollection } from 'astro:content';
+import { OGImageRoute } from 'astro-og-canvas';
+const collectionEntries = await getCollection('posts');
+
+// Map the array of content collection entries to create an object.
+// Converts [{ id: 'post.md', data: { title: 'Example', description: '' } }]
+// to { 'post.md': { title: 'Example', description: '' } }
+const pages = Object.fromEntries(collectionEntries.map(({ id, data }) => [id, data]));
+
+export const { getStaticPaths, GET } = OGImageRoute({
+  // Tell us the name of your dynamic route segment.
+  // In this case it’s `route`, because the file is named `[...route].ts`.
+  param: 'route',
+
+  // A collection of pages to generate images for.
+  // The keys of this object are used to generate the path for that image.
+  // In this example, we generate one image at `/open-graph/example.png`.
+  pages: pages,
+
+  // For each page, this callback will be used to customize the OpenGraph image.
+  getImageOptions: (path, page) => ({
+    title: page.title,
+    description: page.description,
+    logo: {
+      path: `.${page.preview}`,
+      size: [400],
+    }
+  }),
+});
